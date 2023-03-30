efab8f19dcee981732d1e43db7392d6aa6c57687
Gustafson <40609758@live.napier.ac.uk>
2023-03-30 16:36:25 +0100

    end of lab01

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -2,6 +2,7 @@ package com.napier.sem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Booa yah!");
     }
+
 }
